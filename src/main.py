bac8258c249b03ab33d70270c854febfa12f5a2d
studiousgamer <natyavidhanbiswas10@gmail.com>
2022-09-25 22:43:14 +0530

    learn tab

--- src/main.py
+++ src/main.py
@@ -8,6 +8,13 @@ class App:
         self.root.geometry("650x350")
         self.root.resizable(False, False)
 
+        self.learn_tab = CTk.CTkFrame(self.root)
+        self.learn_tab.place(x=0, y=0, width=575, height=350)
+
+        CTk.CTkLabel(self.learn_tab, text="Elements", text_font=("Arial", 18)).place(x=10, y=20, width=120, height=25)
+
+        self.element_list = tk.Listbox(self.learn_tab, background="#525252", borderwidth=0, highlightthickness=0)
+        self.element_list.place(x=10, y=50, width=120, height=280)
 
 
 if __name__ == "__main__":
