3db740d59628ce7aed7ce271997e7a71f11e7f11
studiousgamer <natyavidhanbiswas10@gmail.com>
2022-09-25 22:22:38 +0530

    base

---
+++ src/main.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+import customtkinter as CTk
+
+class App:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Atno")
+        self.root.geometry("650x350")
+        self.root.resizable(False, False)
+
+
+
+if __name__ == "__main__":
+    root = CTk.CTk()
+    app = App(root)
+    root.mainloop()
